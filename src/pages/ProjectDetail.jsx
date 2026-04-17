58f34ada8d81874c06017aec1df19e7c3b53219d
Lokisdp <sodanapallilokeshreddy@gmail.com>
2026-04-17 16:12:03 +0200

    Add app routes and page placeholders

---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,8 @@
+export default function ProjectDetail() {
+  return (
+    <div>
+      <h1>Project Detail Page</h1>
+      <p>This page will be built later.</p>
+    </div>
+  );
+}
---
+++ src/pages/Thesis.jsx
@@ -0,0 +1,8 @@
+export default function Thesis() {
+  return (
+    <div>
+      <h1>Thesis Page</h1>
+      <p>This page will be built later.</p>
+    </div>
+  );
+}
